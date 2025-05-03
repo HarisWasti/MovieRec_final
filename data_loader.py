80b451ae5f818f915dc5566d91c9effd60b97f48
HarisWasti <152641801+HarisWasti@users.noreply.github.com>
2025-05-03 19:23:31 +0100

    Add files via upload

---
+++ data_loader.py
@@ -0,0 +1,35 @@
+import os
+import requests
+import joblib
+import pandas as pd
+
+DATA_DIR = "data"
+os.makedirs(DATA_DIR, exist_ok=True)
+
+FILES = {
+    "movie_meta.csv": "https://1drv.ms/x/c/db1fd89b1b4b1003/EazXL6SxPY1FkTOdmWLSONUBpBgcp5iNAfREHq0SuIvFlQ?e=grKbKJ",
+    "tfidf_matrix.pkl": "https://1drv.ms/u/c/db1fd89b1b4b1003/EVG4OzuqenVNhULl2X6cmBkB7480jSVYAircD2xFFD5dtQ?e=lpZdYy",
+    "user_movie_ratings.pkl": "https://1drv.ms/u/c/db1fd89b1b4b1003/EQ2sE32_Vt1Blsb_jWKSZ4wBxm8vBcEnB3e5rmIlr11hrg?e=okeeTG",
+    "item_movie_matrix.pkl": "https://1drv.ms/u/c/db1fd89b1b4b1003/EZRiwHAWPeBOuMgodFYrWTQBhsBXb8RQXzNJSGCJQQAGCA",
+    "knn_model.pkl": "https://1drv.ms/u/c/db1fd89b1b4b1003/ESrQLm8v_kFGiOXCuJttbdkBGX2sCCZScdcQONZzTvaMxg?e=5IE9HG"
+}
+
+def download_file(name, url):
+    local_path = os.path.join(DATA_DIR, name)
+    if not os.path.exists(local_path):
+        print(f"Downloading {name}...")
+        r = requests.get(url, allow_redirects=True)
+        with open(local_path, 'wb') as f:
+            f.write(r.content)
+    return local_path
+
+def load_all_data():
+    paths = {name: download_file(name, url) for name, url in FILES.items()}
+
+    return {
+        "movie_meta": pd.read_csv(paths["movie_meta.csv"]),
+        "tfidf_matrix": joblib.load(paths["tfidf_matrix.pkl"]),
+        "user_movie_ratings": joblib.load(paths["user_movie_ratings.pkl"]),
+        "item_movie_matrix": joblib.load(paths["item_movie_matrix.pkl"]),
+        "knn": joblib.load(paths["knn_model.pkl"])
+    }
