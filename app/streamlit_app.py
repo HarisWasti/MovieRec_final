f71d9e931cd4513057d6e135f64d5dbe3e26671f
HarisWasti <152641801+HarisWasti@users.noreply.github.com>
2025-05-04 16:26:31 +0100

    Update streamlit_app.py

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -5,12 +5,15 @@ from rec_engine import hybrid_recommendations
 import joblib
 from scipy import sparse
 
-# Load preprocessed data
-movie_meta = pd.read_csv('data/movie_meta.csv')
-tfidf_matrix = joblib.load('data/tfidf_matrix.pkl')
-user_movie_ratings = joblib.load('data/user_movie_ratings.pkl')
-item_movie_matrix = joblib.load('data/item_movie_matrix.pkl')
-knn = joblib.load('data/knn_model.pkl')
+from app.data_loader import load_all_data
+
+data = load_all_data()
+movie_meta = data["movie_meta"]
+tfidf_matrix = data["tfidf_matrix"]
+user_movie_ratings = data["user_movie_ratings"]
+item_movie_matrix = data["item_movie_matrix"]
+knn = data["knn"]
+
 
 st.set_page_config(page_title="Movie Recommender", layout="wide")
 
