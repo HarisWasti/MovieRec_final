9bd099168b7744d9f50312f66a79134b008515b1
HarisWasti <152641801+HarisWasti@users.noreply.github.com>
2025-05-06 03:22:58 +0100

    Update data_loader.py

--- app/data_loader.py
+++ app/data_loader.py
@@ -1,8 +1,15 @@
 import pandas as pd
-import joblib
+from scipy import sparse
+import streamlit as st
 
+@st.cache_resource
 def load_movie_meta():
-    return pd.read_csv("data/movie_meta.csv")
+    return pd.read_parquet("data/movie_meta.parquet")
 
+@st.cache_resource
 def load_tfidf_matrix():
-    return joblib.load("data/tfidf_matrix.pkl")
+    return sparse.load_npz("data/tfidf_matrix.npz")
+
+@st.cache_resource
+def load_item_movie_matrix():
+    return sparse.load_npz("data/item_movie_matrix.npz")
